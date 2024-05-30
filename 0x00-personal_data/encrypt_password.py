9b1bcf90842e7d735b6d947d4c5811ffb46596e5
mouadedd <mdx.edd@gmail.com>
2024-05-30 11:38:12 -0700

    pycodestyle

--- 0x00-personal_data/encrypt_password.py
+++ 0x00-personal_data/encrypt_password.py
@@ -12,5 +12,6 @@ def is_valid(hashed_password: bytes, password: str) -> bool:
     """ checck valid password """
     return bcrypt.checkpw(password.encode('utf-8'), hashed_password)
 
+
 if __name__ == "__main__":
     main()
