7149840b50982b149b254f0c6af77691e0c74898
mouadedd <mdx.edd@gmail.com>
2024-06-06 19:23:48 +0100

    task 6

--- 0x02-Session_authentication/api/v1/auth/session_auth.py
+++ 0x02-Session_authentication/api/v1/auth/session_auth.py
@@ -2,6 +2,7 @@
 """ 1. Empty session """
 from .auth import Auth
 from uuid import uuid4
+from models.user import User
 
 
 class SessionAuth(Auth):
@@ -21,3 +22,10 @@ class SessionAuth(Auth):
         if session_id is None or not isinstance(session_id, str):
             return None
         return self.user_id_by_session_id.get(session_id)
+    
+    def current_user(self, request=None):
+        """6. Use Session ID for identifying a User"""
+        sess_cookie = self.session_cookie(request)
+        u_id = self.user_id_for_session_id(sess_cookie)
+        user =  User.get(u_id)
+        return user
