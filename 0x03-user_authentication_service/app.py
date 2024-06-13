3f1cab52857130c097b681307cf7e45259a66475
mouadedd <mdx.edd@gmail.com>
2024-06-13 15:12:41 +0100

    up

--- 0x03-user_authentication_service/app.py
+++ 0x03-user_authentication_service/app.py
@@ -2,6 +2,10 @@
 """Basic Flask app"""
 from flask import Flask, jsonify
 
+from auth import Auth
+
+AUTH = Auth()
+
 app = Flask(__name__)
 
 
@@ -9,6 +13,17 @@ app = Flask(__name__)
 def index() -> str:
     """basic rout """
     return jsonify({"message": "Bienvenue"})
+
+
+@app.route("/users", mothods=["POST"])
+def users() -> str:
+    """7. Register user"""
+    email, pswd = request.form.get('email'), request.form.get('password')
+    try:
+        user = AUTH.register_user(email, pswd)
+    except ValueError:
+        return jsonify({"message": "email already registered"}), 400
+    return jsonify({"email": "%s" % email, "message": "user created"})
 
 
 if __name__ == "__main__":
